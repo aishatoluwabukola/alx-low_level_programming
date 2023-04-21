94e99499d1ae1420042e6910c66c4fe66a947cb0
Aishat Atobiloye <oluwabukolaatobiloye@gmail.com>
2023-04-21 12:28:31 -0700

    Print to standard output with puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,14 @@
-#!/bin/bash
 #include <stdio.h>
-int main()
+
+/**
+ * main - entry point
+ * input - none
+ *
+ * return value - 0(no error)
+ */
+
+int main(void)
 {
-	puts("\"Programmig is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
